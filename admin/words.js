54fb08e89ee4e883d01d931b5b9b696ea1a40f29
Florian Herrmannsdoerfer <florian.herrmannsdoerfer@scc-group.eu>
2023-01-25 14:24:57 +0100

    init

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'io-link-master-al1370 adapter settings': {
+		'en': 'Adapter settings for io-link-master-al1370',
+		'de': 'Adaptereinstellungen für io-link-master-al1370',
+		'ru': 'Настройки адаптера для io-link-master-al1370',
+		'pt': 'Configurações do adaptador para io-link-master-al1370',
+		'nl': 'Adapterinstellingen voor io-link-master-al1370',
+		'fr': "Paramètres d'adaptateur pour io-link-master-al1370",
+		'it': "Impostazioni dell'adattatore per io-link-master-al1370",
+		'es': 'Ajustes del adaptador para io-link-master-al1370',
+		'pl': 'Ustawienia adaptera dla io-link-master-al1370',
+		'zh-cn': 'io-link-master-al1370的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
